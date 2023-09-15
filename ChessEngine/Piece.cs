e5d80f7764aa24e8ac85e83c1f30e4bf9d5f198c
Sam <sammydean57@gmail.com>
2023-09-15 16:13:06 +0100

    Piece class

---
+++ ChessEngine/Piece.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessEngine
+{
+    public enum PieceType
+    {
+        pawn = 0,
+        knight = 1,
+        bishop = 2,
+        rook = 3,
+        queen = 4,
+        king = 5
+    }
+    public enum Colour
+    {
+        white,
+        black
+    }
+    public class Piece
+    {
+        public PieceType Type { get; }
+        public Colour Colour { get; }
+        public Piece(PieceType type, Colour colour)
+        {
+            Type = type;
+            Colour = colour;
+        }
+    }
+}
